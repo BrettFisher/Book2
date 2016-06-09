0283e7868071c0c17f2c471abb60e2953bfbc9e5
Brett Fisher <Brett.Fisher@dcsg.com>
2016-06-09 09:56:31 -0400

    Started book three with project called MyLocations.

---
+++ MyLocations/MyLocations/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyLocations
+//
+//  Created by Fisher, Brett on 6/9/16.
+//  Copyright © 2016 Fisher, Brett. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController
+{
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
---
+++ MyLocations/MyLocations/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MyLocations
+//
+//  Created by Fisher, Brett on 6/9/16.
+//  Copyright © 2016 Fisher, Brett. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
